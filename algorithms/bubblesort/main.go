97b98a20a07e2d847aac09bf849b197bb6ce0268
Gabriel Pozo <jackgris2@gmail.com>
2023-05-18 13:48:16 -0300

    refactor: fibonacci and bubblesort algorithms

--- algorithms/bubblesort/main.go
+++ algorithms/bubblesort/main.go
@@ -4,29 +4,26 @@ import "fmt"
 
 // Running bubble sort solution
 func main() {
-	var integers [11]int = [11]int{31, 13, 12, 4, 18, 16, 7, 2, 3, 0, 10}
+	integers := []int{31, 13, 32, 12, 4, 18, 16, 7, 2, 3, 0, 10}
 	fmt.Println("Bubble Sorter")
-	bubbleSorter(integers)
+	fmt.Println("Original: ", integers)
+	fmt.Println("Result:   ", bubbleSorter(integers))
 }
 
 // bubble Sorter method
-func bubbleSorter(integers [11]int) {
-
-	num := 11
-	var isSwapped bool
-	isSwapped = true
+func bubbleSorter(integers []int) []int {
+	isSwapped := true
 	for isSwapped {
 		isSwapped = false
-		var i int
-		for i = 1; i < num; i++ {
+		for i := 1; i < len(integers); i++ {
 			if integers[i-1] > integers[i] {
 
-				var temp = integers[i]
+				temp := integers[i]
 				integers[i] = integers[i-1]
 				integers[i-1] = temp
 				isSwapped = true
 			}
 		}
 	}
-	fmt.Println(integers)
+	return integers
 }
--- algorithms/fibonacci/main.go
+++ algorithms/fibonacci/main.go
@@ -2,42 +2,41 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 // Running fibonacci solutions
 func main() {
-	var i int
-	for i = 0; i <= 9; i++ {
-		fmt.Print(strconv.Itoa(series(i)) + " ")
+	fibo := 9
+	seriesResult := []int{}
+	for i := 0; i <= fibo; i++ {
+		seriesResult = append(seriesResult, series(i))
 	}
-	fmt.Println("")
-	for i = 0; i <= 9; i++ {
-		fmt.Print(strconv.Itoa(fibonacciNumber(i)) + " ")
+	fmt.Println("Series:    ", seriesResult)
+	recursiveResult := []int{}
+	for i := 0; i <= fibo; i++ {
+		recursiveResult = append(recursiveResult, recursive(i))
 	}
-	fmt.Println("")
+	fmt.Println("Recursive: ", recursiveResult)
 }
 
 // series method
 func series(n int) int {
-	var f []int
-	f = make([]int, n+1, n+2)
+	f := make([]int, n+1, n+2)
 	if n < 2 {
 		f = f[0:2]
 	}
 	f[0] = 0
 	f[1] = 1
-	var i int
-	for i = 2; i <= n; i++ {
+	for i := 2; i <= n; i++ {
 		f[i] = f[i-1] + f[i-2]
 	}
 	return f[n]
 }
 
-// fibonacciNumber method
-func fibonacciNumber(n int) int {
+// recursive method
+func recursive(n int) int {
 	if n <= 1 {
 		return n
 	}
-	return fibonacciNumber(n-1) + fibonacciNumber(n-2)
+	return recursive(n-1) + recursive(n-2)
 }
