928d576e7b6813677675cbe97850d3f4f282640c
Gabriel Pozo <jackgris2@gmail.com>
2025-06-08 14:49:15 -0300

    add: array max value

---
+++ algorithms/max-value/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func max(arrays []int, length int) int {
+	for i := 0; i < length-1; i++ {
+		if arrays[i] > arrays[i+1] { // swap
+			var temp = arrays[i]
+			arrays[i] = arrays[i+1]
+			arrays[i+1] = temp
+		}
+	}
+	var maxValue = arrays[length-1]
+	return maxValue
+}
+func main() {
+	var scores = []int{60, 50, 95, 80, 70}
+	var length = len(scores)
+	var maxValue = max(scores, length)
+	fmt.Printf("Max Value = %d\n", maxValue)
+}
